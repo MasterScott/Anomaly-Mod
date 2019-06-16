9a360f8968bdbffdd28696d6fd6d664953f0b732
Pure <codystevenson123@gmail.com>
2019-06-16 02:27:19 -0700

    Changed to Proper URL / XPATH. Fixed the date issue

--- OpenBullet/Pages/Main/Settings/OB/OBSettingsSources.xaml.cs
+++ OpenBullet/Pages/Main/Settings/OB/OBSettingsSources.xaml.cs
@@ -13,8 +13,11 @@ namespace OpenBullet
     {
         private ViewModels.OBSettingsSources vm;
         private Random rand = new Random();
+
+
+
         //Gets Update URL
-        private string UpdateDateURL = "https://github.com/PurityWasHere/Anomaly-Mod-Hosting/blob/master/Update.zip";
+        private string UpdateDateURL = "https://github.com/PurityWasHere/Anomaly-Mod-Hosting/blob/master/Configs.zip";
 
 
         public OBSettingsSources()
