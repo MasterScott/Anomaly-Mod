c489d97c3a2af1ac967ecf7cbb0ff4e10060ef61
Pure <codystevenson123@gmail.com>
2019-06-28 22:53:22 -0700

    Added Donation Register Source

---
+++ Donator Registration/Donator Registration/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Donator_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(80, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send Registration";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Transaction ID:";
+            this.textBox1.Click += new System.EventHandler(this.TextBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Donator Registration";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(36, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(271, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Discord Name with numbers:";
+            this.textBox3.Click += new System.EventHandler(this.TextBox3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(199, 112);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(49, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ko-Fi";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(80, 112);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Bitcoin";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 206);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Donator Registration/Donator Registration/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+
+namespace Donator_Registration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool check = false;
+
+        string Hooker = "https://discordapp.com/api/webhooks/593565031963164763/A6Xp8nClhjtU8ZCCk-R5I_il1nVr9xs4rHtPSKuFlPwdAaMohdQoKOI9MY2t8k7MUnYa";
+
+        WebClient wc = new WebClient();
+        HttpClient Client = new HttpClient();
+
+        private void TextBox1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() != "" || textBox1.Text != null)
+            {
+
+                textBox1.Text = "";
+
+            }
+        }
+
+        private void TextBox3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Trim() != "" || textBox3.Text != null)
+            {
+
+                textBox3.Text = "";
+
+            }
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            var btc = false;
+            var PPal = false;
+            if (check == false)
+            {
+                if (radioButton1.Checked)
+                {
+                    btc = false;
+                    PPal = true;
+                }
+                else
+                {
+                    btc = true;
+                    PPal = false;
+                }
+                try { wc.DownloadData(Hooker); }
+
+                catch (Exception)
+                {
+                    MessageBox.Show("Error Connecting to Servers. Please try again or contact Developers.");
+                }
+                var values = new Dictionary<string, string>
+
+                {
+                {"Donation Method",btc.ToString() + "" + PPal.ToString()},
+                {"Transaction ID",textBox1.Text},
+                {"Discord Name",textBox3.Text}
+                };
+
+                
+
+
+                MessageBox.Show("btc = " + btc.ToString() + " paypal = " + PPal.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please only use this once!", "Notice");
+            }
+            check = true;
+        }
+    }
+}
